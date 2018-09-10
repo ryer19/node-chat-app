3509574071e269f027778d5f009a6774d8512215
Robert Smiles Jr <smilesr120@gmail.com>
2018-09-10 16:54:51 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath))
+
+app.get('/', () => {
+
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
