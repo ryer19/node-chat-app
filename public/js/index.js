18c420dae065cc2b370c56488674dffe3f63b7db
Robert Smiles Jr <smilesr120@gmail.com>
2018-09-18 15:04:40 -0400

    added user help if Safari geolocation turned off

--- public/js/index.js
+++ public/js/index.js
@@ -88,8 +88,11 @@ locationButton.addEventListener('click', function (e) {
       longitude: position.coords.longitude,
     })
   }, function (err) {
-    alert(JSON.stringify(err))
-  })
+    if (JSON.stringify(err) === "{}") {
+      alert('You will need to allow geolocation on your browser.  If you are using Safari, go to Settings>>Privacy>>Location Services>>Safari')
+    } else {
+      alert(JSON.stringify(err))
+    })
   console.log('yippee')
   // socket.emit('createLocationMessage', {
   //   blah: 'blah'
